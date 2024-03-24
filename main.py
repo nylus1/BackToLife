59769195a7688a6e489b1d635607ae6ee3b08326
nylus1 <ivb.ignacio@gmail.com>
2024-03-24 18:05:36 +0100

    pollon cipotastico

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-import os
+def leer_disco(ruta):
+    try:
+        with open(ruta, 'rb') as f:
+            while True:
+                bloque = f.read(1024)
+                if not bloque:
+                    break
+                for b in bloque:
+                    print(f'{b:02x}', end=' ')
+                print()
+    except PermissionError:
+        print("No tienes permisos suficientes para acceder a este disco.")
+
+leer_disco('E:') 
